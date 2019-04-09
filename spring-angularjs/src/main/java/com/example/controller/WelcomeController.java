8f383c59f458da61990e818c9b066ddb75ef04dd
pavankunasani <pavankunasani@users.noreply.github.com>
2019-04-09 16:46:15 +0530

    second commit

--- spring-angularjs/src/main/java/com/example/controller/WelcomeController.java
+++ spring-angularjs/src/main/java/com/example/controller/WelcomeController.java
@@ -1,10 +1,20 @@
 package com.example.controller;
 
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
 
-@RestController
+
+@Controller
 public class WelcomeController {
 
+	
+	
+	@GetMapping({"/", "/hello"})
+    public String hello(Model model) {
+        model.addAttribute("name", "world");
+        return "hello";
+    }
 	
 	
 }
